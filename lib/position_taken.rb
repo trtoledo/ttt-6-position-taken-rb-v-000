ec32122fcd1613ad6e6153f000ceadd1d60f0a8d
Tomas Toledo <trtoledo1977@gmail.com>
2019-10-12 02:22:20 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,3 +7,7 @@ def position_taken?(board, index)
   end
 end
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+end
